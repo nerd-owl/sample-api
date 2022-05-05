9bb6ec8617ebad76b4eb0123f9dd9d5b18992102
savez siddiqui <savezsiddiqui@savezsiddiqui-MBP.local>
2022-05-05 16:36:44 +0530

    feat: add sqlc queries

---
+++ db/query/user.sql
@@ -0,0 +1,11 @@
+-- name: ListUser :many
+SELECT * FROM kuser;
+
+-- name: CreateUser :exec
+INSERT INTO kuser (FirstName, LastName, Phone, Addr)
+VALUES ($1, $2, $3, $4);
+
+-- name: DeactivateUser :exec
+UPDATE kuser
+SET Active = False
+WHERE Phone = $1;
